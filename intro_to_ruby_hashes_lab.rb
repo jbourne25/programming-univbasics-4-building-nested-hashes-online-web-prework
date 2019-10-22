dd9d4bbc7c12cb9e242f9a6a3520bca43719cd77
Jeremy Bourne <jbourne25@yahoo.com>
2019-10-22 19:12:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,10 @@ def monopoly_with_third_tier
    hash = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {}
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_piece_owned => 50,
+        :three_piece_owned => 
     }
   }
 end 
