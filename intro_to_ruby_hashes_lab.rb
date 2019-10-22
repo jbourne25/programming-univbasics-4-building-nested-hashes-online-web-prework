293808f25acf622a804a8acd9ce9e08a581baed6
Jeremy Bourne <jbourne25@yahoo.com>
2019-10-22 17:56:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	first_hash = {:hash => "no return"}
+	first_hash = {:railroads => "no return"}
 	first_hash
 	
 	  
