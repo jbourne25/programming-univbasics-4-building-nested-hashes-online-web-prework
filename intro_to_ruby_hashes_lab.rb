9754775a8ff61f8dcc52204bf4a44a6d0bc75f93
Jeremy Bourne <jbourne25@yahoo.com>
2019-10-22 19:53:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,9 +41,8 @@ def monopoly_with_third_tier
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
-      }
+     }
     }
-  }
   hash
 end 
 #monopoly_with_third_tier[:railroads] = (:rent_in_dollars)
